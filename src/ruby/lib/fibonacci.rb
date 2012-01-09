bf7280f5e280cc8047b57174c322cffa9fc39c18
Shawn Wallace <shawn@wshawn.com>
2012-01-09 18:52:29 -0500

    Begin Get Ready for Code PaLOUsa 2012

--- src/ruby/lib/fibonacci.rb
+++ src/ruby/lib/fibonacci.rb
@@ -9,21 +9,21 @@ module Fibonacci
 
     return old
   end
-  
+
   def self.ofRecursion(fib)
     return fib if (0..1).include? fib
-    
+
     return ofRecursion(fib - 1) +  ofRecursion(fib - 2)
   end
-  
+
   def self.of(fib)
     (
       ((phi**fib) - (1.0 - phi)**fib) / Math.sqrt(5)
     ).floor
   end
-  
+
   def self.phi
     @phi ||= (1.0 + Math.sqrt(5)) / 2
   end
-	
+
 end
