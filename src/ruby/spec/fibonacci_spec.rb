43ca94af091e32129741f14027374e1a8bf940f0
Shawn Wallace <shawn@wshawn.com>
2012-03-08 07:48:32 -0500

    polished off final demo for Code PaLOUsa

--- src/ruby/spec/fibonacci_spec.rb
+++ src/ruby/spec/fibonacci_spec.rb
@@ -11,23 +11,23 @@ describe Fibonacci do
     [17, 1597],
     [45, 1134903170],
     [83, 99194853094755497]
-    ]
+  ]
 
   @values.each do |fib, expected|
     it "should be #{expected} if #{fib} computed iteratively" do
       Fibonacci.ofIterative(fib).should == expected
     end
   end
-
-  # @values.each do |fib, expected|
-  #   it "should be #{expected} if #{fib} computed recursively" do
-  #     #Fibonacci.ofRecursion(fib).should == expected
-  #   end
-  # end
-
-  # @values.each do |fib, expected|
-  #   it "should be #{expected} if #{fib} computed optimally" do
-  #     Fibonacci.of(fib).should == expected
-  #   end
-  # end
+# 
+#   @values.each do |fib, expected|
+#     it "should be #{expected} if #{fib} computed recursively" do
+#       #Fibonacci.ofRecursion(fib).should == expected
+#     end
+#   end
+# 
+#   @values.each do |fib, expected|
+#     it "should be #{expected} if #{fib} computed optimally" do
+#       Fibonacci.of(fib).should == expected
+#     end
+#   end
 end
